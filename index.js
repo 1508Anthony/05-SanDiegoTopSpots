8edf68595bf14c44cac70944aa4e931c9aa0bee4
1508Anthony <admin@admins-MBP-2.home>
2016-06-12 16:18:55 -0700

    Initial commit - no functionality

---
+++ index.js
@@ -0,0 +1,23 @@
+$(function() {
+	// TODO - fill in the JS functionality
+});
+
+// Create a folder named 05-SanDiegoTopSpots in your dev folder.
+// Setup your Git workflow.
+// Initialize an empty git repository in 05-SanDiegoTopspots by running git
+// init in the command prompt.
+// Create a repository on GitHub called 05-SanDiegoTopspots and follow the
+// instructions to add a remote origin.
+// Open this folder in your favorite text editor (Ours is Sublime!)
+// Create an index.html file and a corresponding index.js file.
+// Download topspots.json to your 05-SanDiegoTopspots folder.
+// Create an HTML table structure with the headers you see in the image above.
+// Also make sure you reference your index.js before the </body> tag.
+// Write the following JavaScript in your index.js file
+// Use the $.getJSON method to download the contents of topspots.json
+// Iterate through the top spots and create rows for each spot.
+// Create a link to the location using the longitude and latitude provided.
+// (Example https://www.google.com/maps?q=33.09745,-116.99572)
+// To start/test your application - navigate to 05-SanDiegoTopSpots in command
+// line and run static . to start a web server that you can access by going to
+// http://localhost:8080.
